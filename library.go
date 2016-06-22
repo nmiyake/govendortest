5cd8cb7c6be4861bdae3d0b61a47aa00d12bec29
Nick Miyake <nmiyake@palantir.com>
2016-06-22 13:44:04 -0700

    Added asset

--- library.go
+++ library.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+  "fmt"
   "os"
 
   "github.com/urfave/cli"
+  "github.com/jteeuwen/go-bindata"
 )
 
 func main() {
   cli.NewApp().Run(os.Args)
+  asset := bindata.Asset{}
+  fmt.Println(asset)
 }
